92539eb606fcfc8d14ff76b78c3ca9d83a926e5b
Robert Tiempo <jroberttiempo58@gmail.com>
2021-10-06 17:01:43 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { AppBar, Box, Toolbar, Button, IconButton, Typography } from '@mui/material';
+import { Navbar, Hero } from './components';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AppBar sx={{ backgroundColor: "#ffffff", boxShadow: 'none',
+      borderBottom: '1px solid rgba(0, 0, 0, 0.12)' }} position="static">
+        <Toolbar>
+          <Typography color='#2CB1BC' variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            u w u
+          </Typography>          
+          <Button variant='text'>About</Button>
+          <Button variant='text'>Skills</Button>
+          <Button variant='text'>Projects</Button>
+          <Button variant='text'>Contact</Button>
+          <Button variant='text'>Resume</Button>
+        </Toolbar>
+      </AppBar>
+      <Navbar />
+      <Hero />
+    </>
   );
 }
 
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AppBar, Box, Toolbar, Button, IconButton, Typography } from '@mui/material';
+
+const Navbar = () => {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default Navbar;
