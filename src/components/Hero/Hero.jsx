b9f2051a09c92e4707354f3dd00715ec80662add
Robert Tiempo <jroberttiempo58@gmail.com>
2022-02-17 09:48:49 +0800

    Fix go to top button not working

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,17 +4,29 @@ import { HeroData } from '../../assets/data/data';
 import { Fade } from 'react-reveal';
 
 const Hero = () => {
-    return (
-        <>
-            <Stack p={3} direction='column' justifyContent='center'  sx={{ minHeight: '100vh' }}>
-                <Fade left duration={750} delay={500} distance="30px">
-                    <Typography variant='h5' color='#044E54'>Hi, my name is</Typography>
-                    <Typography variant='h3' color='#14919B'>{HeroData.name}.</Typography>
-                    <Typography variant='h4' color='#102A43'>I'm a {HeroData.title}.</Typography>
-                </Fade>
-            </Stack>                
-        </>
-    )
-}
+  return (
+    <>
+      <Stack
+        id="Top"
+        p={3}
+        direction="column"
+        justifyContent="center"
+        sx={{ minHeight: '100vh' }}
+      >
+        <Fade left duration={750} delay={500} distance="30px">
+          <Typography variant="h5" color="#044E54">
+            Hi, my name is
+          </Typography>
+          <Typography variant="h3" color="#14919B">
+            {HeroData.name}.
+          </Typography>
+          <Typography variant="h4" color="#102A43">
+            I'm a {HeroData.title}.
+          </Typography>
+        </Fade>
+      </Stack>
+    </>
+  );
+};
 
 export default Hero;
