efcdaa801715079ed80272e5bbcf1223709576a6
Robert Tiempo <jroberttiempo58@gmail.com>
2022-02-17 09:25:53 +0800

    Update skills data

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -1,57 +1,49 @@
 export const HeroData = {
-    name: 'John Robert Tiempo',
-    title: 'full stack developer'
+  name: 'John Robert Tiempo',
+  title: 'full stack developer',
 };
 
 export const AboutData = {
-    body: "I'm a hard-working computer science student with a flair for creating effective and efficient solutions in the least amount of time. Has experience in web development both in front end and back end. Able to grasp new concepts quickly and efficiently. Successful working in both team and self-directed settings."
+  body: "I'm a hard-working computer science student with a flair for creating effective and efficient solutions in the least amount of time. Has experience in web development both in front end and back end. Able to grasp new concepts quickly and efficiently. Successful working in both team and self-directed settings.",
 };
 
 export const SkillsData = {
-    languages: [
-        'Javascript',
-        'HTML',
-        'CSS',
-        'SQL',
-        'Dart'
-    ],
-    libraries: [
-        'React',
-        'Node.js',
-        'Express',
-        'Material UI',
-        'Styled Components',
-        'Flutter'
-    ],
-    tools: [
-        'Git',
-        'Firebase',
-        'Figma'
-    ]
+  languages: ['Javascript', 'Typescript', 'HTML', 'CSS', 'SQL', 'Dart'],
+  libraries: [
+    'React',
+    'Node.js',
+    'Express',
+    'Nest.js',
+    'Material UI',
+    'Styled Components',
+    'Flutter',
+  ],
+  tools: ['MongoDB', 'Git', 'Firebase', 'Figma'],
 };
 
 export const ProjectsData = [
-    {
-        title: 'ChatApp',
-        img: 'https://i.ibb.co/jyq7Jfd/chatapp-comp.jpg',
-        stack: ['Dart', 'Flutter'],
-        info: 'A messaging mobile app built using Flutter and Firebase.',
-        url: '', // if url is empty then no button will appear
-        repo: 'https://github.com/rtiempo/ChatApp-Tiempo' // if repo is empty then no button will appear
-    },
-    {
-        title: 'Ecom',
-        img: 'https://i.ibb.co/Wzdnyww/ecom-comp.jpg',
-        stack: ['Javascript', 'React', 'Material UI', 'Commerce.js', 'Stripe'],
-        info: 'A basic Ecommerce webapp built using React.',
-        url: 'https://ecomtiempo.netlify.app/', // if url is empty then no button will appear
-        repo: 'https://github.com/rtiempo/ecom' // if repo is empty then no button will appear
-    },
+  {
+    title: 'ChatApp',
+    img: 'https://i.ibb.co/jyq7Jfd/chatapp-comp.jpg',
+    stack: ['Dart', 'Flutter'],
+    info: 'A messaging mobile app built using Flutter and Firebase.',
+    url: '', // if url is empty then no button will appear
+    repo: 'https://github.com/rtiempo/ChatApp-Tiempo', // if repo is empty then no button will appear
+  },
+  {
+    title: 'Ecom',
+    img: 'https://i.ibb.co/Wzdnyww/ecom-comp.jpg',
+    stack: ['Javascript', 'React', 'Material UI', 'Commerce.js', 'Stripe'],
+    info: 'A basic Ecommerce webapp built using React.',
+    url: 'https://ecomtiempo.netlify.app/', // if url is empty then no button will appear
+    repo: 'https://github.com/rtiempo/ecom', // if repo is empty then no button will appear
+  },
 ];
 
 export const ContactData = {
-    email: 'jroberttiempo58@gmail.com',
-    github: 'https://github.com/rtiempo',
-    linkedIn: 'https://www.linkedin.com/in/robert-tiempo/', // if linkedIn is empty then no button will appear
-    resume: 'https://drive.google.com/file/d/1ygikiatfsb9QK163mqN1eMtuhI6MQJlR/view?usp=sharing'
+  email: 'jroberttiempo58@gmail.com',
+  github: 'https://github.com/rtiempo',
+  linkedIn: 'https://www.linkedin.com/in/robert-tiempo/', // if linkedIn is empty then no button will appear
+  resume:
+    'https://drive.google.com/file/d/1ygikiatfsb9QK163mqN1eMtuhI6MQJlR/view?usp=sharing',
 };
